a8ddee21e82bf53545fc9ccaa12725e002be229a
Steven Duran <stevenduran@gmail.com>
2022-04-26 18:04:09 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -49,10 +49,12 @@ class Dev(Configuration):
       'django.contrib.staticfiles',
       'crispy_forms',
       'crispy_bootstrap5',
+      "debug_toolbar",
       'blog',
   ]
 
   MIDDLEWARE = [
+      "debug_toolbar.middleware.DebugToolbarMiddleware",
       'django.middleware.security.SecurityMiddleware',
       'django.contrib.sessions.middleware.SessionMiddleware',
       'django.middleware.common.CommonMiddleware',
@@ -186,6 +188,8 @@ class Dev(Configuration):
         "level": "DEBUG",
     },
 }
+
+  INTERNAL_IPS = ["192.168.10.93"]
 
 class Prod(Dev):
   DEBUG = values.BooleanValue(True)
--- blango/urls.py
+++ blango/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import debug_toolbar
+from django.conf import settings
+
 import blog.views
 
 
@@ -22,4 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("post/<slug>/", blog.views.post_detail, name = "blog-post-detail"),
     path("", blog.views.index),
+    path("ip/", blog.views.get_ip)
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
